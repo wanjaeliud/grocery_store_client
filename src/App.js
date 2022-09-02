81774441167267f45e15e9b8d1e2f153c82c0514
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-09-02 17:52:22 +0300

    create routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 import Navbar from './components/header/Navbar';
+import {Routes, Route} from 'react-router-dom';
+import Products from './components/products/Products';
+import Reviews from './components/reviews/Reviews';
+import Blogs from './components/blogs/Blogs';
 
 function App() {
   return (
-      <p>Grocery Store</p>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={ <Home/> } />
+          <Route path="about" element={ <Products/> } />
+          <Route path="contact" element={ <Reviews/> } />
+          <Route path="/blog" element={<Blogs />} />
+        </Routes>
+      </div>
   );
 }
 
