f5a326fd8e971386a797b0639020c268aeac1599
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-09-03 16:03:08 +0300

    nav-bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
 import Navbar from './components/header/Navbar';
-import {Routes, Route} from 'react-router-dom';
-import Products from './components/products/Products';
-import Reviews from './components/reviews/Reviews';
-import Blogs from './components/blogs/Blogs';
+
 
 function App() {
   return (
       <div className="App">
-        <Routes>
-          <Route path="/" element={ <Home/> } />
-          <Route path="about" element={ <Products/> } />
-          <Route path="contact" element={ <Reviews/> } />
-          <Route path="/blog" element={<Blogs />} />
-        </Routes>
+        <Navbar/>
       </div>
   );
 }
