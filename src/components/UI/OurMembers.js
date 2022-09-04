4d9fc556df90ac695084bfa6211d7afa404564b0
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-04 13:03:12 +0300

    add members details

--- src/components/UI/OurMembers.js
+++ src/components/UI/OurMembers.js
@@ -1,9 +1,52 @@
 import React from "react";
-import "../../components/UI/our-member.css";
+import "../../components/UI/member.css";
 import {col} from "reactstrap";
 import {Link} from "react-router-dom"
 import ava1 from "../../assets/images/ava1";
 import ava2 from "../../assets/images/ava2";
 import ava3 from "../../assets/images/ava3";
 import ava4 from "../../assets/images/ava4";
-import ava5 from "../../assets/images/ava5"
+// import ava5 from "../../assets/images/ava5"
+
+const OUR_MEMBERS = [
+    {
+        name: "JOHN OKETCH",
+        experience: "5 years of experience",
+        fbUrl: "#",
+        instUrl: "#",
+        twitUrl: "#",
+        linkedinUrl: "#",
+        imgUrl: ava1,
+      },
+    
+      {
+        name: "OLOVIA ADONGO",
+        experience: "5 years of experience",
+        fbUrl: "#",
+        instUrl: "#",
+        twitUrl: "#",
+        linkedinUrl: "#",
+        imgUrl: ava2,
+      },
+    
+      {
+        name: "ELIUD WANJA",
+        experience: "5 years of experience",
+        fbUrl: "#",
+        instUrl: "#",
+        twitUrl: "#",
+        linkedinUrl: "#",
+        imgUrl: ava3,
+      },
+    
+      {
+        name: "ANJELA KANYI",
+        experience: "5 years of experience",
+        fbUrl: "#",
+        instUrl: "#",
+        twitUrl: "#",
+        linkedinUrl: "#",
+        imgUrl: ava4,
+    },
+];
+
