893812387dc0047a7d2e4fa7ddd2dbf695dbb0b7
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-04 12:54:42 +0300

    iport members images

---
+++ src/components/UI/OurMembers.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../../components/UI/our-member.css";
+import {col} from "reactstrap";
+import {Link} from "react-router-dom"
+import ava1 from "../../assets/images/ava1";
+import ava2 from "../../assets/images/ava2";
+import ava3 from "../../assets/images/ava3";
+import ava4 from "../../assets/images/ava4";
+import ava5 from "../../assets/images/ava5"
