20c3591822946b13222121c582b883c5dd2f519d
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-09-04 16:48:08 +0300

    home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,15 +3,16 @@ import React from 'react';
 function Home(props) {
   return (
       <div className="banner">
-        <div className="card bg-dark text-dark border-0">
-          <img src="/images/banner.png" className="card-img" alt="..."/>
-            <div className="card-img-overlay">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a wider card with supporting text
-                below as a natural lead-in to additional content. This content
-                is a little bit longer.</p>
-              <p className="card-text">Last updated 3 mins ago</p>
+        <div className="card bg-dark text-white border-0">
+          <img src="/images/banner.png" className="card-img" alt="Page banner"/>
+          <div className="card-img-overlay d-flex flex-column justify-content-center">
+            <div className="container">
+              <h5 className="card-title display-4 fw-bolder mb-0">FRESH PRODUCTS
+                FROM YOUR FARM</h5>
+              <p className="card-text lead text-black fw-bold display-6">We connects Farmers to the Market <br/> without
+                the Middle Man</p>
             </div>
+          </div>
         </div>
       </div>
   );
