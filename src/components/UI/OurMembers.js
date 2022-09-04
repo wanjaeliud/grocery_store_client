b23d2fd90d93b68802ea8edefc1d7f5033b27308
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-04 13:08:00 +0300

    mapping through the members details

--- src/components/UI/OurMembers.js
+++ src/components/UI/OurMembers.js
@@ -50,3 +50,43 @@ const OUR_MEMBERS = [
     },
 ];
 
+const OurMembers = () => {
+    return (
+      <>
+        {OUR_MEMBERS.map((item, index) => (
+          <col lg="3" md="3" sm="4" xs="6" key={index} className="mb-4">
+            <div className="single__member">
+              <div className="single__member-img">
+                <img src={item.imgUrl} alt="" className="w-100" />
+  
+                <div className="single__member-social">
+                  <Link to={item.fbUrl}>
+                    <i class="ri-facebook-line"></i>
+                  </Link>
+                  <Link to={item.twitUrl}>
+                    <i class="ri-twitter-line"></i>
+                  </Link>
+  
+                  <Link to={item.linkedinUrl}>
+                    <i class="ri-linkedin-line"></i>
+                  </Link>
+  
+                  <Link to={item.instUrl}>
+                    <i class="ri-instagram-line"></i>
+                  </Link>
+                </div>
+              </div>
+  
+              <h6 className="text-center mb-0 mt-3">{item.name}</h6>
+              <p className="section__description text-center">
+                {item.experience}
+              </p>
+            </div>
+          </col>
+        ))}
+      </>
+    );
+  };
+  
+  export default OurMembers;
+
