26df3c24c82b99b5de91f5f268d72fd66f8ef766
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-04 14:46:43 +0300

    add image and import about page

--- src/components/about us/About.js
+++ src/components/about us/About.js
@@ -1,3 +1,7 @@
 import React from "react";
 import { Container,Row, Col } from "reactstrap";
 import "../../design/about.css";
+import CommonSection from "../UI/CommonSection";
+import AboutSection from "../UI/AboutSection";
+import goatimage from "../../design/goatimage";
+import OurMembers from "../../components/UI/OurMembers";
