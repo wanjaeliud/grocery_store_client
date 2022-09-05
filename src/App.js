49b914aa59a85242926fb400f72086863d833a32
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-09-05 09:54:50 +0300

    Pulls changes from miriam branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import Navbar from './components/header/Navbar';
 import Events from "./components/events/Events"
-
-
+import Home from "./components/home/Home"
 
 
 function App() {
