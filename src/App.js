e18d85a3d4662c2fa000da1a7061174a46b07cd6
Miriam Anyango <minaoanya@gmail.com>
2022-09-05 09:40:14 +0300

    Make blog posts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import './App.css';
 import Navbar from './components/header/Navbar';
+import Blogs from './components/blogs/Blogs';
+// import Home from './components/home/Home';
 
+
+function App() {
+  return (
+      <div className="App">
+    <Navbar />
+        <Blogs />
+        {/* <Navbar/> */}
+        {/* <Home/> */}
+      </div>
+  );
+}
 
 export default App;
--- src/components/blogs/Post/Post.js
+++ src/components/blogs/Post/Post.js
@@ -26,7 +26,121 @@ export default function Post() {
                        remaining essentially unchanged.</p>
                   </div>
               </div>
-
+          </div>
+          <div className="myposts">
+              <div className="image">
+              <img 
+              className="postImg"
+              src="https://www.greenlife.co.ke/wp-content/uploads/2021/09/Spinach_nutrition.jpg"
+              alt=""
+              />
+              </div>
+              <div className="content">
+                  <div className="postCat">
+                    <span className="postCat">Organic</span>
+                    <span className="postCat">LifeStyle</span>
+                  </div>
+                  <div>
+                      <h3>Title</h3>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                       when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                       It has survived not only five centuries, but also the leap into electronic typesetting,
+                       remaining essentially unchanged.</p>
+                  </div>
+              </div>
+          </div>
+          <div className="myposts">
+              <div className="image">
+              <img 
+              className="postImg"
+              src="https://media-cdn.tripadvisor.com/media/photo-s/01/6a/6f/f6/fresh-produce-truck-canoa.jpg"
+              alt=""
+              />
+              </div>
+              <div className="content">
+                  <div className="postCat">
+                    <span className="postCat">Organic</span>
+                    <span className="postCat">LifeStyle</span>
+                  </div>
+                  <div>
+                      <h3>Title</h3>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                       when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                       It has survived not only five centuries, but also the leap into electronic typesetting,
+                       remaining essentially unchanged.</p>
+                  </div>
+              </div>
+          </div>
+          <div className="myposts">
+              <div className="image">
+              <img 
+              className="postImg"
+              src="https://stmaaprodfwsite.blob.core.windows.net/assets/sites/1/pest-2-Black-bean-aphids-on-bean-stem-c-Blackthorn-Arable.jpg"
+              alt=""
+              />
+              </div>
+              <div className="content">
+                  <div className="postCat">
+                    <span className="postCat">Organic</span>
+                    <span className="postCat">LifeStyle</span>
+                  </div>
+                  <div>
+                      <h3>Title</h3>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                       when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                       It has survived not only five centuries, but also the leap into electronic typesetting,
+                       remaining essentially unchanged.</p>
+                  </div>
+              </div>
+          </div>
+          <div className="myposts">
+              <div className="image">
+              <img 
+              className="postImg"
+              src="http://cdn.standardmedia.co.ke/images/friday/thumb_safe_methods_for_pes5dbc34a400afc.jpg"
+              alt=""
+              />
+              </div>
+              <div className="content">
+                  <div className="postCat">
+                    <span className="postCat">Organic</span>
+                    <span className="postCat">LifeStyle</span>
+                  </div>
+                  <div>
+                      <h3>Title</h3>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                       when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                       It has survived not only five centuries, but also the leap into electronic typesetting,
+                       remaining essentially unchanged.</p>
+                  </div>
+              </div>
+          </div>
+          <div className="myposts">
+              <div className="image">
+              <img 
+              className="postImg"
+              src="https://www.agfoundation.org/images/uploads/_660w/corn-harvest-5596982_1920.jpg"
+              alt=""
+              />
+              </div>
+              <div className="content">
+                  <div className="postCat">
+                    <span className="postCat">Organic</span>
+                    <span className="postCat">LifeStyle</span>
+                  </div>
+                  <div>
+                      <h3>Title</h3>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                       when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                       It has survived not only five centuries, but also the leap into electronic typesetting,
+                       remaining essentially unchanged.</p>
+                  </div>
+              </div>
           </div>
 
       </div>
