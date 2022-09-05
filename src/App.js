0de1a2bfe44a2613c15e23aa06f58079ab7ab2e9
Olivia Adongo <akinyioliviatove@gmail.com>
2022-09-05 10:19:24 +0300

    Api added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { productData } from './components/products/data';
 import Navbar from './components/header/Navbar';
-import Products from './components/products/Products';
+import Products from './components/products';
+
 
 function App() {
   return (
     <div>
-    <Products heading= "Fresh and healthy produce from our trusted farmers" data={productData}/>
+    <Products heading= "Favourite product" data={productData}/>
     </div>
      
   );
